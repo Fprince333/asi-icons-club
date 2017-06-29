b2e9ad0a84a4d29d848de9b4ccd911125dd72374
Fprince333 <lwilsonsmith@gmail.com>
2017-06-29 17:01:49 -0400

    Add Pardot form handler and update css

--- custom.js
+++ custom.js
@@ -1,17 +1,42 @@
-jQuery(document).ready(function() {
-  const addBackground = $j(".tiled-background").length;
-  if (addBackground) {
-    let imageUrl = `http://iconsclub.archsystems.com/wp-content/uploads/2017/06/Artboard-1-e1498313654166.jpg`;
-    $j("body").addClass("membership-form");
-    $j(".wrapper").css("background-image", "url(" + imageUrl + ")");
-    $j(".wrapper").css("background-size", "100% auto");
-    $j(".wrapper").css("background-repeat", "no-repeat");
-    $j(".wrapper").css("height", "100%");
-    $j(".wrapper").css("width", "100vw");
-    $j(".full_width").css("background-color", "black");
-    $j(".wrapper_inner").css("padding-top", "10vh");
-    $j(".wrapper_inner").css("padding-bottom", "10vh");
-  } else {
-    console.log("Don't add background");
-  }
+// Pardot form handler
+jQuery(document).ready(function($) {
+  // set the pardot form handler url and form element id
+  const partdotFormHandlerURL =
+    "http://www2.archsystems.com/l/67312/2017-06-26/9zsvcx";
+  const formElementID = "um_form_memberregistration";
+  const firstName = $("#um_field_3_memberregistration").val();
+  const lastName = $("#um_field_4_memberregistration").val();
+  const title = $("#um_field_6_memberregistration").val();
+  const company = $("#um_field_8_memberregistration").val();
+  const email = $("#um_field_9_memberregistration").val();
+
+  $("body").append(
+    '<iframe id="pardot-form-handler" height="0" width="0" style="position:absolute; left:-100000px; top:-100000px" src="javascript:false;"></iframe>'
+  );
+  $("#" + formElementID).attr(
+    "action",
+    "javascript:postToPardot('" + $("#" + formElementID).attr("action") + "')"
+  );
 });
+
+function postToPardot(formAction) {
+  $("#pardot-form-handler").load(function() {
+    $("#" + formElementID).attr("action", formAction);
+    $("#" + formElementID).submit();
+  });
+  $("#pardot-form-handler").attr(
+    "src",
+    partdotFormHandlerURL +
+      "?" +
+      "email=" +
+      email +
+      "&first_name=" +
+      firstName +
+      "&last_name=" +
+      lastName +
+      "&title=" +
+      title +
+      "&company=" +
+      company
+  );
+}
