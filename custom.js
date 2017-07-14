1923d8f912841f11a0e065a72e48dca803a8d71a
Fprince333 <lwilsonsmith@gmail.com>
2017-07-14 14:26:40 -0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,9 +1,24 @@
 $j(document).ready(function() {
   const hutchQuote = $j(".yt-hutchinson");
+  const daimiQuote = $j(".yt-daimi");
+  const cobenQuote = $j(".yt-coben");
+
   if (hutchQuote.length > 0) {
     const icon = hutchQuote.find(".mpc-icon");
     icon.css("cursor", "pointer");
-    icon.on("click", playYouTubeVideo);
+    icon.on("click", linkToYouTubeVideoOfHutch);
+  }
+
+  if (daimiQuote.length > 0) {
+    const icon = daimiQuote.find(".mpc-icon");
+    icon.css("cursor", "pointer");
+    icon.on("click", linkToYouTubeVideoOfDaimi);
+  }
+
+  if (cobenQuote.length > 0) {
+    const icon = cobenQuote.find(".mpc-icon");
+    icon.css("cursor", "pointer");
+    icon.on("click", linkToYouTubeVideoOfCoben);
   }
 });
 
@@ -12,4 +27,19 @@ function playYouTubeVideo() {
   $j(".yt-hutchinson").find(".mpc-icon").css("border-radius", "0");
   $j(".yt-hutchinson").find(".mpc-icon").css("border", "none");
   $j(".yt-hutchinson").find(".mpc-icon").html(youTubeIframe);
+}
+
+function linkToYouTubeVideoOfHutch() {
+  let youTubeLink = `https://youtu.be/0c0stsGBtnM`;
+  window.open(youTubeLink);
+}
+
+function linkToYouTubeVideoOfDaimi() {
+  let youTubeLink = `https://youtu.be/rtxYieiz8ok`;
+  window.open(youTubeLink);
+}
+
+function linkToYouTubeVideoOfCoben() {
+  let youTubeLink = `https://youtu.be/kMhaZ05jPFE`;
+  window.open(youTubeLink);
 }
